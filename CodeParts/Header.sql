1d21ed8c2a91748c2a94427e5bd7f09a516d9256
Micael Uthas <micael.uthas@nefab.com>
2024-01-26 10:24:50 +0100

    Modifications GOD, rebuild and rename. Modifications to Create Sp, Tbl, Trg

---
+++ CodeParts/Header.sql
@@ -0,0 +1,13 @@
+﻿/*
+	Enter a complete query in @SelectStmnt.
+	Enter a prefix for the columns if you need one.
+
+	!!Add a WHERE 1 = 2 CLAUSE to avoid data loading.
+
+	Generates:
+	1. ColumnList for the columns in the SELECT Statement.
+	2. DECLARE statements for variables representing the columns in the SELECT Statement.
+	3. SET variable (@ColVar = Col) values from the columns in the SELECT Statement.
+
+	Author:Micael Uthas (uthas.com)
+*/
